cf4b5376ea5317626e7024315abdce11a0b9dd9d
Hank <l945457758@gmail.com>
2018-08-28 20:47:32 +0800

    更新几种错误类型

--- src/service/ExceptionService.java
+++ src/service/ExceptionService.java
@@ -7,11 +7,15 @@ import org.springframework.stereotype.Service;
 public interface ExceptionService {
 
     //_user 是从数据库中查找出的记录，user 是用户输入
-    public void loginException(User user, User db_user) throws UserException;
+    void loginException(User user, User db_user) throws UserException;
 
-    public void addUserException1(User user) throws UserException;
+    void addUserException1(User user) throws UserException;
 
-    public void addUserException2(User user) throws UserException;
+    void addUserException2(User user) throws UserException;
 
-    public void verifyCodeException(String input_code, String code) throws UserException;
+    void verifyCodeException(String input_code, String code) throws UserException;
+
+    void setInfoException(User user) throws UserException;
+
+    void statusException(String username) throws UserException;
 }
