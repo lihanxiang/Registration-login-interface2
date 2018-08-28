de7ad62e8a6786d2d187a704ac871c8987a2f24b
Hank <l945457758@gmail.com>
2018-08-28 20:47:51 +0800

    更新几个功能

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -4,7 +4,13 @@ import config.exception.UserException;
 import domain.User;
 import util.VerifyCode;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface UserService {
-    public void addUser(User user) throws UserException;
-    public void login(User user) throws UserException;
+    void addUser(User user) throws UserException;
+    void login(User user) throws UserException;
+    User showInfo(String username);
+    String getStatus(String username) throws UserException;
+    void setUserInfo(User user) throws UserException;
+    void verifyCode(String userCode, String verifyCode) throws UserException;
 }
