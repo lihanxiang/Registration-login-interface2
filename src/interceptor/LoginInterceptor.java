fdaa86ad18e3de6b50ee4b9f1b29460818ab86cd
Hank <l945457758@gmail.com>
2018-08-31 21:35:23 +0800

    拦截器

---
+++ src/interceptor/LoginInterceptor.java
@@ -0,0 +1,33 @@
+package interceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements org.springframework.web.servlet.HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest,
+                             HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession session = httpServletRequest.getSession();
+        if (session.getAttribute("username") == null){
+            httpServletResponse.sendRedirect("preLogin.action");
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                           Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest,
+                                HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
